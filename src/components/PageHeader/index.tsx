f3f177fb876bb14bf7be8d7dac49dad5c3e00775
MarllonCampos <marlloncampos@outlook.com>
2020-08-09 18:40:40 -0300

    Criado mais uma prop para o pageheader (headerRight) que verificara se os filtros estao ativos

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,32 +1,42 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { View, Image, Text } from "react-native";
-import {BorderlessButton} from 'react-native-gesture-handler'
+import { BorderlessButton } from "react-native-gesture-handler";
 
-import backIcon from '../../assets/images/icons/back.png'
-import logoImg from '../../assets/images/logo.png'
+import backIcon from "../../assets/images/icons/back.png";
+import logoImg from "../../assets/images/logo.png";
 
 import styles from "./styles";
 import { useNavigation } from "@react-navigation/native";
 
-interface PageHeaderProps{
-  title:string
+interface PageHeaderProps {
+  title: string;
+  headerRight?: ReactNode;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({title}) => {
-  const {navigate} = useNavigation();
-  function handleGoBack(){
-      navigate('Landing')
+const PageHeader: React.FC<PageHeaderProps> = ({
+  title,
+  headerRight,
+  children,
+}) => {
+  const { navigate } = useNavigation();
+  function handleGoBack() {
+    navigate("Landing");
   }
   return (
     <View style={styles.container}>
       <View style={styles.topBar}>
         <BorderlessButton onPress={handleGoBack}>
-          <Image source={backIcon} resizeMode='contain'/>
+          <Image source={backIcon} resizeMode="contain" />
         </BorderlessButton>
-          <Image source={logoImg} resizeMode='contain'/>
+        <Image source={logoImg} resizeMode="contain" />
       </View>
 
-      <Text style={styles.title}>{title}</Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>{title}</Text>
+        {headerRight}
+      </View>
+
+      {children}
     </View>
   );
 };
