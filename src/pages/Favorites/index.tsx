fa3df751bca32e7108094535ad54c0ab9add3aab
MarllonCampos <marlloncampos@outlook.com>
2020-08-09 18:42:39 -0300

    Inserido o component teacherItem na pagina e tambem criado um scrollview para os mesmos components

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,14 +1,27 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
 
 import styles from "./styles";
 import PageHeader from "../../components/PageHeader";
-
+import TeacherItem from "../../components/TeacherItem";
 
 function Favorites() {
-  return <View style={styles.container}>
-    <PageHeader title="Meus proffys favoritos " />
-  </View>;
+  return (
+    <View style={styles.container}>
+      <PageHeader title="Meus proffys favoritos " />
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 16 }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
+    </View>
+  );
 }
 
 export default Favorites;
