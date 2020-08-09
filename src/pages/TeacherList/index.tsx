4a4bb2c56586b413699290163502fae33535aca0
MarllonCampos <marlloncampos@outlook.com>
2020-08-09 19:20:45 -0300

    Criado estados para controlar, a materia,semana,hora e tambem para maniupula a montagem do component teacherItem em tela

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -5,20 +5,33 @@ import { Feather } from "@expo/vector-icons";
 
 import styles from "./styles";
 import PageHeader from "../../components/PageHeader";
-import TeacherItem from "../../components/TeacherItem";
+import TeacherItem, { Teacher } from "../../components/TeacherItem";
+import api from "../../services/api";
 
 function TeacherList() {
+  const [teachers, setTeachers] = useState([]);
   const [isFiltersVisible, setIsFiltersVisible] = useState(false);
+  const [subject, setSubject] = useState("");
+  const [week_day, setWeekDay] = useState("");
+  const [time, setTime] = useState("");
 
   function handleToggleFiltersVisible() {
     setIsFiltersVisible(!isFiltersVisible);
+  }
+  async function handleFiltersSubmit() {
+   const response =  await api.get("/classes", { params: { subject, week_day, time } });
+
+   setIsFiltersVisible(false);
+
+    setTeachers(response.data)
+
   }
 
   return (
     <View style={styles.container}>
       <PageHeader
         title="Proffys disponíveis"
-        headerRight={(
+        headerRight={
           <BorderlessButton>
             <Feather
               name="filter"
@@ -26,7 +39,7 @@ function TeacherList() {
               color="#fff"
               onPress={handleToggleFiltersVisible}
             />
-          </BorderlessButton>)
+          </BorderlessButton>
         }
       >
         {isFiltersVisible && (
@@ -34,6 +47,10 @@ function TeacherList() {
             <Text style={styles.label}>Matéria</Text>
             <TextInput
               style={styles.input}
+              value={subject}
+              onChangeText={(text) => {
+                setSubject(text);
+              }}
               placeholder="Qual a matéria?"
               placeholderTextColor="#C1BCCC"
             />
@@ -43,6 +60,10 @@ function TeacherList() {
                 <Text style={styles.label}>Dia da semana</Text>
                 <TextInput
                   style={styles.input}
+                  value={week_day}
+                  onChangeText={(text) => {
+                    setWeekDay(text);
+                  }}
                   placeholder="Qual o dia??"
                   placeholderTextColor="#C1BCCC"
                 />
@@ -52,12 +73,21 @@ function TeacherList() {
                 <Text style={styles.label}>Horário</Text>
                 <TextInput
                   style={styles.input}
+                  value={time}
+                  onChangeText={(text) => {
+                    setTime(text);
+                  }}
                   placeholder="Qual horário?"
                   placeholderTextColor="#C1BCCC"
                 />
               </View>
             </View>
-            <RectButton style={styles.submitButton}><Text style={styles.submitButtonText}>Enviar</Text></RectButton>
+            <RectButton
+              style={styles.submitButton}
+              onPress={handleFiltersSubmit}
+            >
+              <Text style={styles.submitButtonText}>Enviar</Text>
+            </RectButton>
           </View>
         )}
       </PageHeader>
@@ -65,12 +95,10 @@ function TeacherList() {
         style={styles.teacherList}
         contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 16 }}
       >
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+
+      {teachers.map((teacher: Teacher) => {
+        return(<TeacherItem key={teacher.id} teacher={teacher} />)
+      })}
       </ScrollView>
     </View>
   );
