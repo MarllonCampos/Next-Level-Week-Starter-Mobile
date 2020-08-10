e0ba156e79d0b38d7df0dbcaa4c30f67ef5f42be
MarllonCampos <marlloncampos@outlook.com>
2020-08-09 22:00:14 -0300

    Criado funcao para carregar os favoritos na pagina e ela carrega os favoritos e chamando essa funcao dentro da funçao de toggle do submit

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -3,28 +3,50 @@ import { View, Text, ScrollView, TextInput } from "react-native";
 import { BorderlessButton, RectButton } from "react-native-gesture-handler";
 import { Feather } from "@expo/vector-icons";
 
-import styles from "./styles";
+import AsyncStorage from "@react-native-community/async-storage";
+
 import PageHeader from "../../components/PageHeader";
 import TeacherItem, { Teacher } from "../../components/TeacherItem";
+
 import api from "../../services/api";
+
+import styles from "./styles";
 
 function TeacherList() {
   const [teachers, setTeachers] = useState([]);
+  const [favorites, setFavorites] = useState<number[]>([]);
   const [isFiltersVisible, setIsFiltersVisible] = useState(false);
   const [subject, setSubject] = useState("");
   const [week_day, setWeekDay] = useState("");
   const [time, setTime] = useState("");
 
+  function loadFavorites() {
+    AsyncStorage.getItem("favorites").then((response) => {
+      if (response) {
+        const favoritedTeachers = JSON.parse(response);
+        const favoritedTeachersIds = favoritedTeachers.map(
+          (teacher: Teacher) => {
+            return teacher.id;
+          }
+        );
+
+        setFavorites(favoritedTeachersIds);
+      }
+    });
+  }
+
   function handleToggleFiltersVisible() {
+    loadFavorites();
     setIsFiltersVisible(!isFiltersVisible);
   }
   async function handleFiltersSubmit() {
-   const response =  await api.get("/classes", { params: { subject, week_day, time } });
+    const response = await api.get("/classes", {
+      params: { subject, week_day, time },
+    });
 
-   setIsFiltersVisible(false);
+    setIsFiltersVisible(false);
 
-    setTeachers(response.data)
-
+    setTeachers(response.data);
   }
 
   return (
@@ -95,10 +117,15 @@ function TeacherList() {
         style={styles.teacherList}
         contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 16 }}
       >
-
-      {teachers.map((teacher: Teacher) => {
-        return(<TeacherItem key={teacher.id} teacher={teacher} />)
-      })}
+        {teachers.map((teacher: Teacher) => {
+          return (
+            <TeacherItem
+              key={teacher.id}
+              teacher={teacher}
+              favorited={favorites.includes(teacher.id)}
+            />
+          );
+        })}
       </ScrollView>
     </View>
   );
