ec4eaba56b8dcba4d7cd6c0dbbf8bc0bd13d97a9
MarllonCampos <marlloncampos@outlook.com>
2020-08-10 10:40:21 -0300

    Criado uma funcao para carregar os favoritos de dentro do AsyncStorage e setFavorites no estado favorites, criado useFocusEffect react.callback para solucionar um looping que ocorria enquanto estava na tela de favoritos

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,11 +1,37 @@
-import { View, Text, ScrollView } from "react-native";
-import React from "react";
+import { View, ScrollView } from "react-native";
+import React, { useState, useEffect } from "react";
+import { useFocusEffect } from "@react-navigation/native";
 
 import styles from "./styles";
+
 import PageHeader from "../../components/PageHeader";
-import TeacherItem from "../../components/TeacherItem";
+import TeacherItem, { Teacher } from "../../components/TeacherItem";
+import AsyncStorage from "@react-native-community/async-storage";
 
 function Favorites() {
+  const [favorites, setFavorites] = useState([]);
+
+  function loadFavorites() {
+    AsyncStorage.getItem("favorites").then((response) => {
+      if (response) {
+        const favoritedTeachers = JSON.parse(response);
+        const favoritedTeachersIds = favoritedTeachers.map(
+          (teacher: Teacher) => {
+            return teacher.id;
+          }
+        );
+
+        setFavorites(favoritedTeachers);
+      }
+    });
+  }
+
+  useFocusEffect(
+    React.useCallback(() => {
+      loadFavorites();
+    }, [])
+  );
+
   return (
     <View style={styles.container}>
       <PageHeader title="Meus proffys favoritos " />
@@ -13,15 +39,11 @@ function Favorites() {
         style={styles.teacherList}
         contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 16 }}
       >
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        {favorites.map((teachers: Teacher) => {
+          return <TeacherItem key={teachers.id} teacher={teachers} favorited />;
+        })}
       </ScrollView>
     </View>
   );
 }
-
 export default Favorites;
