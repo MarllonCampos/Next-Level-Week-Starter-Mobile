061fcaffc6abe8c7a1f154bfef550028dcdeb7ce
MarllonCampos <marlloncampos@outlook.com>
2020-08-09 21:58:18 -0300

    importado async storage para lidar com armazenamento local do celular,Criado novo estado para gerencias os professores favoritos, criado funcao para abrir o whatsapp por meio do deep link, criado funcao assincrona para lidar com o toggle dos favoritos

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,43 +1,72 @@
-import React from "react";
-import { View, Text, Image } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Image, Linking } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
+import AsyncStorage from "@react-native-community/async-storage";
 
-import styles from "./styles";
 import heartOutlineIcon from "../../assets/images/icons/heart-outline.png";
 import unfavoriteIcon from "../../assets/images/icons/unfavorite.png";
 import whatsappIcon from "../../assets/images/icons/whatsapp.png";
 
-export interface Teacher{
-  id:number,
-  avatar:string,
-  bio:string,
-  cost:number,
-  name:string,
-  subject:string,
-  whatsapp:string
+import styles from "./styles";
+
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
 }
-interface TeacherItemProps{
-  teacher:Teacher
+interface TeacherItemProps {
+  teacher: Teacher;
+  favorited: boolean;
 }
 
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, favorited }) => {
+  const [isFavorited, setIsFavorited] = useState(favorited);
 
-const TeacherItem:React.FC<TeacherItemProps> = ({teacher}) => {
+  function handleLinkToWhatsapp() {
+    Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
+  }
+
+  async function handleToggleFavorite() {
+    const favorites = await AsyncStorage.getItem("favorites");
+
+    let favoritesArray = [];
+
+    if (favorites) {
+      favoritesArray = JSON.parse(favorites);
+    }
+
+    if (isFavorited) {
+      const favoriteIndex = favoritesArray.findIndex((teacherItem: Teacher) => {
+        return teacherItem.id === teacher.id;
+      });
+
+      setIsFavorited(false)
+      favoritesArray.splice(favoriteIndex, 1);
+    } else {
+      console.log(favoritesArray);
+
+      favoritesArray.push(teacher);
+
+      setIsFavorited(true);
+    }
+    await AsyncStorage.setItem("favorites", JSON.stringify(favoritesArray));
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.profile}>
-        <Image
-          style={styles.avatar}
-          source={{ uri: teacher.avatar }}
-        />
+        <Image style={styles.avatar} source={{ uri: teacher.avatar }} />
 
         <View style={styles.profileInfo}>
           <Text style={styles.name}> {teacher.name}</Text>
           <Text style={styles.subject}> {teacher.subject}</Text>
         </View>
       </View>
-      <Text style={styles.bio}>
-        {teacher.bio}
-      </Text>
+      <Text style={styles.bio}>{teacher.bio}</Text>
 
       <View style={styles.footer}>
         <Text style={styles.price}>
@@ -46,12 +75,21 @@ const TeacherItem:React.FC<TeacherItemProps> = ({teacher}) => {
         </Text>
 
         <View style={styles.buttonsContainer}>
-          <RectButton style={[styles.favoriteButton, styles.favorited]}>
-            {/* <Image source={heartOutlineIcon} /> */}
-            <Image source={unfavoriteIcon} />
+          <RectButton
+            style={[styles.favoriteButton, isFavorited ? styles.favorited : {}]}
+            onPress={handleToggleFavorite}
+          >
+            {isFavorited ? (
+              <Image source={unfavoriteIcon} />
+            ) : (
+              <Image source={heartOutlineIcon} />
+            )}
           </RectButton>
 
-          <RectButton style={styles.contactButton}>
+          <RectButton
+            style={styles.contactButton}
+            onPress={handleLinkToWhatsapp}
+          >
             <Image source={whatsappIcon} />
             <Text style={styles.contactButtonText}>Entrar em contato</Text>
           </RectButton>
@@ -59,6 +97,6 @@ const TeacherItem:React.FC<TeacherItemProps> = ({teacher}) => {
       </View>
     </View>
   );
-}
+};
 
 export default TeacherItem;
